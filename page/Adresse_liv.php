f82b9848a177b2a8eba9b56fbf3812902a170166
Mateo Luque <117266260+Sqwado@users.noreply.github.com>
2023-12-06 23:18:24 +0100

    Update Adresse_liv.php

--- page/Adresse_liv.php
+++ page/Adresse_liv.php
@@ -39,7 +39,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/adresse_liv.css">
+    <link rel="stylesheet" href="/style/Adresse_liv.css">
     <title>Addrese livraison</title>
 </head>
 
